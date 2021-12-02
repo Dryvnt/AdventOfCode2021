86b457641078a3bfe0e146542cf36deec62e90c5
Thomas Gravgaard Hansen <tgh@gravgaard.dev>
2021-12-02 18:30:56 +0100

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input");
+
+var depth = 0;
+var horizontal = 0;
+foreach (var line in lines)
+{
+    var words = line.Split(" ");
+    var direction = words[0];
+    var distance = int.Parse(words[1]);
+    switch (words[0])
+    {
+        case "forward":
+            horizontal += distance;
+            break;
+        case "down":
+            depth += distance;
+            break;
+        case "up":
+            depth -= distance;
+            break;
+    }
+}
+
+Console.WriteLine($"Part one - Depth: {depth}, Horizontal: {horizontal}, Answer: {depth * horizontal}");
+
+depth = 0;
+horizontal = 0;
+var aim = 0;
+foreach (var line in lines)
+{
+    var words = line.Split(" ");
+    var direction = words[0];
+    var distance = int.Parse(words[1]);
+    switch (words[0])
+    {
+        case "forward":
+            horizontal += distance;
+            depth += aim * distance;
+            break;
+        case "down":
+            aim += distance;
+            break;
+        case "up":
+            aim -= distance;
+            break;
+    }
+}
+
+Console.WriteLine($"Part Two - Depth: {depth}, Horizontal: {horizontal}, Aim: {aim}, Answer: {depth * horizontal}");
