a2e356e3d344248009d600dde195ef6ca1a87a53
Thomas Gravgaard Hansen <tgh@gravgaard.dev>
2021-12-12 22:43:01 +0100

    Faster implementation of Day 12
    Signed-off-by: Thomas Gravgaard Hansen <tgh@gravgaard.dev>

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,74 +1,73 @@
-﻿using System.Collections.Immutable;
+﻿using System.Diagnostics;
 
-bool Part1Condition(SearchEntry entry)
+int AllPaths(Dictionary<string, HashSet<string>> dictionary, Part part)
 {
-    var (node, path) = entry;
-    if (node.ToLowerInvariant() != node) return true;
-
-    var seen = new HashSet<string>();
-
-    foreach (var n in path.Where(n => n == n.ToLowerInvariant()))
-    {
-        if (seen.Contains(n)) return false;
-
-        seen.Add(n);
-    }
-
-    return true;
-}
-
-bool Part2Condition(SearchEntry entry)
-{
-    var (node, path) = entry;
-    if (node.ToLowerInvariant() != node) return true;
-
-    var seen = new HashSet<string>();
-
-    var special = (string?)null;
-
-    foreach (var n in path.Where(n => n == n.ToLowerInvariant()))
-    {
-        if (seen.Contains(n))
-        {
-            if (n is "start" or "end") return false;
-            if (special is null) special = n;
-            else return false;
-        }
-
-        seen.Add(n);
-    }
-
-    return true;
-}
-
-// This is a very brute-force-y solution.
-// I'm sure there's a smarter way to do it, but at this point it's late and I'm tired.
-int DoPart(IDictionary<string, HashSet<string>> adjacencyDict, Func<SearchEntry, bool> conditionFunction)
-{
-    var allPaths = new List<ImmutableList<string>>();
-
     var stack = new Stack<SearchEntry>();
-    stack.Push(new SearchEntry("start", new[] { "start" }.ToImmutableList()));
+
+    var specials = new[] { (string?)null };
+
+    if (part == Part.Two)
+        specials = specials
+            .Concat(dictionary.Keys.Where(n => n.ToLowerInvariant() == n).Except(new[] { "start", "end" })).ToArray();
+
+    var startPath = new[] { "start" }.ToList();
+    var allLegalVertices = dictionary.Keys.Except(new[] { "start" }).ToHashSet();
+    stack.Push(new SearchEntry("start", startPath, allLegalVertices, new HashSet<string>(), false));
+
+    var list = new List<List<string>>();
+
     while (stack.Any())
     {
         var v = stack.Pop();
 
         if (v.Node == "end")
         {
-            allPaths.Add(v.Path);
+            list.Add(v.Path);
             continue;
         }
 
-        var neighbors = adjacencyDict[v.Node];
 
-        var neighborEntries = neighbors
-            .Select(n => new SearchEntry(n, v.Path.Add(n)))
-            .Where(conditionFunction);
+        var neighbors = dictionary[v.Node].Where(v.LegalMoves.Contains);
 
-        foreach (var newEntry in neighborEntries) stack.Push(newEntry);
+        foreach (var n in neighbors)
+        {
+            var newLegal = v.LegalMoves;
+            var exceptionMade = v.Part2ExceptionMade;
+            var newUsed = v.UsedSmall;
+            if (n.ToLowerInvariant() == n)
+            {
+                switch (part)
+                {
+                    case Part.One:
+                    case Part.Two when exceptionMade:
+                        newLegal = newLegal.ToHashSet();
+                        newLegal.Remove(n);
+                        break;
+                    case Part.Two:
+                    {
+                        if (v.Path.Contains(n))
+                        {
+                            newLegal = newLegal.Except(newUsed).ToHashSet();
+                            exceptionMade = true;
+                        }
+
+                        break;
+                    }
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(part), part, null);
+                }
+
+                newUsed = newUsed.ToHashSet();
+                newUsed.Add(n);
+            }
+
+            var newPath = v.Path.ToList();
+            newPath.Add(n);
+            stack.Push(new SearchEntry(n, newPath, newLegal, newUsed, exceptionMade));
+        }
     }
 
-    return allPaths.Count;
+    return list.Count;
 }
 
 var input = File.ReadAllLines("input");
@@ -89,7 +88,22 @@ foreach (var line in input)
     adjacencyDict[to].Add(from);
 }
 
-Console.WriteLine($"Part 1: {DoPart(adjacencyDict, Part1Condition)}");
-Console.WriteLine($"Part 2: {DoPart(adjacencyDict, Part2Condition)}");
+var c = new Stopwatch();
+c.Start();
+Console.WriteLine($"Part 1: {AllPaths(adjacencyDict, Part.One)}");
+Console.WriteLine($"Part 2: {AllPaths(adjacencyDict, Part.Two)}");
+c.Stop();
+Console.WriteLine($"Elapsed time: {c.Elapsed}");
 
-internal record SearchEntry(string Node, ImmutableList<string> Path);
+// I benchmarked it, and the non-immutable data structures are faster than the immutable ones,
+// despite the immutable ergonomics being more intuitive for this sort of "progressively build
+// the data structure down the tree" workflow. Oh well.
+
+internal record SearchEntry(string Node, List<string> Path, HashSet<string> LegalMoves, HashSet<string> UsedSmall,
+    bool Part2ExceptionMade);
+
+internal enum Part
+{
+    One,
+    Two,
+}
