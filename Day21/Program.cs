1ce4a9577e359f5217fdac9216c0f0b0cc6775b7
Thomas Gravgaard Hansen <tgh@gravgaard.dev>
2021-12-21 16:38:01 +0100

    Day 21
    Signed-off-by: Thomas Gravgaard Hansen <tgh@gravgaard.dev>

---
+++ Day21/Program.cs
@@ -0,0 +1,104 @@
+﻿int PlayDeterministic(Player player1, Player player2, int goal)
+{
+    var rolls = 0;
+    while (true)
+    {
+        var r1 = rolls * 3 + 6;
+        rolls += 3;
+
+        player1 = player1.Move(r1);
+        if (player1.Score >= goal) return rolls * player2.Score;
+
+        var r2 = rolls * 3 + 6;
+        rolls += 3;
+
+        player2 = player2.Move(r2);
+        if (player2.Score >= goal) return rolls * player1.Score;
+    }
+}
+
+var input = File.ReadAllLines("input");
+
+var positions = input.Select(l => int.Parse(l.Split(' ').Last())).ToList();
+var player1 = new Player(positions[0] - 1);
+var player2 = new Player(positions[1] - 1);
+
+Console.WriteLine($"Part 1: {PlayDeterministic(player1, player2, 1000)}");
+
+var memoizer = new MemoizedDirac();
+var part2 = memoizer.PlayDirac(player1, player2, 21);
+
+Console.WriteLine($"Part 2: {new[] { part2.Player1Wins, part2.Player2Wins }.Max()}");
+Console.WriteLine($"We explored {memoizer.Count} unique universes to find this answer!");
+Console.WriteLine(part2);
+
+internal class MemoizedDirac
+{
+    private readonly Dictionary<(Player, Player, int), DiracResult> _cache = new();
+
+    private readonly (int Roll, long Count)[] _rolls =
+    {
+        (3, 1),
+        (4, 3),
+        (5, 6),
+        (6, 7),
+        (7, 6),
+        (8, 3),
+        (9, 1),
+    };
+
+    public int Count => _cache.Count;
+
+    public DiracResult PlayDirac(Player player1, Player player2, int goal)
+    {
+        if (_cache.TryGetValue((player1, player2, goal), out var cache)) return cache;
+
+        var player1Wins = 0L;
+        var player2Wins = 0L;
+
+        foreach (var (roll, count) in _rolls)
+        {
+            var afterRoll = player1.Move(roll);
+            if (afterRoll.Score >= goal)
+            {
+                player1Wins += count;
+                continue;
+            }
+
+            var minScore = Math.Min(afterRoll.Score, player2.Score);
+
+            // Note: players are flipped in sub-game!
+            var subGameResult = PlayDirac(player2.SubScore(minScore), afterRoll.SubScore(minScore), goal - minScore);
+
+            player1Wins += subGameResult.Player2Wins * count;
+            player2Wins += subGameResult.Player1Wins * count;
+        }
+
+        var result = new DiracResult(player1Wins, player2Wins);
+
+        _cache[(player1, player2, goal)] = result;
+
+        return result;
+    }
+}
+
+internal readonly record struct Player(int Position, int Score = 0)
+{
+    public Player Move(int roll)
+    {
+        var newPos = (Position + roll) % 10;
+        var newScore = Score + newPos + 1;
+
+        return new Player(newPos, newScore);
+    }
+
+    public Player SubScore(int sub)
+    {
+        return this with
+        {
+            Score = Score - sub,
+        };
+    }
+}
+
+internal readonly record struct DiracResult(long Player1Wins, long Player2Wins);
